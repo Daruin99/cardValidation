0dc8f08cf65ba81842381bc42253a392d79a2830
Daruin99 <102298637+Daruin99@users.noreply.github.com>
2024-07-09 13:25:08 +0300

    create  repo

---
+++ cardValidation/src/test/java/com/cardValidation/cardValidation/CardValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardValidation.cardValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
