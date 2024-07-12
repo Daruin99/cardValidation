0581271497e9a4cbdd67efc6d3fd9e5483334583
Daruin99 <102298637+Daruin99@users.noreply.github.com>
2024-07-12 21:12:01 +0300

    Modify CORS

--- cardValidation/src/main/java/com/cardValidation/cardValidation/controller/CardValidationController.java
+++ cardValidation/src/main/java/com/cardValidation/cardValidation/controller/CardValidationController.java
@@ -5,10 +5,7 @@ import com.cardValidation.cardValidation.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/card-validation")
@@ -21,8 +18,10 @@ public class CardValidationController {
         this.cardService = cardService;
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("/validate")
     public ResponseEntity<String> validateCard(@RequestBody Card cardDetails) {
+        System.out.println("validate");
         Card card = cardService.findByCardNumber(cardDetails.getCardNumber());
         if (card != null && card.getExpiryDate().equals(cardDetails.getExpiryDate()) && card.getCvv().equals(cardDetails.getCvv())) {
             if (cardService.isExpiryDateValid(card)) {
